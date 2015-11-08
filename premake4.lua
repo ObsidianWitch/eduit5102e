11417bdfcdc7f1378141de0d132abe73ef5df280
Obside <obsiagae@gmail.com>
2015-11-08 17:38:52 +0100

    simplify premake script

--- premake4.lua
+++ premake4.lua
@@ -9,12 +9,7 @@ solution "IT5102E_OpenGL_Project"
         includedirs { "src" }
         files {
             "src/*.cpp", "src/*.hpp",
-            "src/inputs/*.cpp", "src/inputs/*.hpp",
-            "src/models/*.cpp", "src/models/*.hpp",
-            "src/shaders/*.cpp", "src/shaders/*.hpp",
-            "src/camera/*.cpp", "src/camera/*.hpp",
-            "src/entities/*.cpp", "src/entities/*.hpp",
-            "src/tools/*.cpp", "src/tools/*.hpp"
+            "src/**/*.cpp", "src/**/*.hpp"
         }
         location "build"
         links { "glfw", "GL", "GLEW", "assimp", "SOIL" }
