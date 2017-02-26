73ee6ec409da0061de73ef727e5dbc48d8d06dec
Obsidienne <obsidienne@protonmail.com>
2017-02-26 17:30:27 +0100

    merge Collision into Collidable

--- src/behaviours/collisions/Collidable.cpp
+++ src/behaviours/collisions/Collidable.cpp
@@ -1,14 +1,19 @@
 #include <glm/glm.hpp>
 
 #include "behaviours/collisions/Collidable.hpp"
-#include "behaviours/collisions/Collision.hpp"
 
 Collidable::Collidable(const BoundingBox& boundingBox) :
     boundingBox(boundingBox)
 {}
 
 bool Collidable::collide(Collidable& b) {
-    return Collision::check(getBoundingBox(), b.getBoundingBox());
+    const auto& box1 = getBoundingBox();
+    const auto& box2 = b.getBoundingBox();
+
+    return (
+        glm::all(glm::greaterThan(box1.maxPos, box2.minPos)) &&
+        glm::all(glm::lessThan(box1.minPos, box2.maxPos))
+    );
 }
 
 BoundingBox Collidable::getBoundingBox() {
--- src/behaviours/collisions/Collision.cpp
+++
@@ -1,8 +0,0 @@
-#include "behaviours/collisions/Collision.hpp"
-
-bool Collision::check(const BoundingBox& box1, const BoundingBox& box2) {
-    return (
-        glm::all(glm::greaterThan(box1.maxPos, box2.minPos)) &&
-        glm::all(glm::lessThan(box1.minPos, box2.maxPos))
-    );
-}
